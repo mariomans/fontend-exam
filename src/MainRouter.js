69255b9fce88c512aeb3d6cc80fdc8671e9b307e
marios <frong_98_@hotmail.com>
2021-01-12 23:12:35 +0700

    Add path route of  Each function

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -3,6 +3,9 @@ import { Route, Switch } from 'react-router-dom'
 import Menu from "./Core/Menu";
 import Signup from "./User/Signup"
 import Pokemon from "./Pokemon/Pokemon"
+import Function1 from "./Function1/Function1"
+import Function2 from "./Function2/Function2"
+import Function3 from "./Function3/Function3"
 
 const MainRouter = () => (
     <div>
@@ -10,6 +13,9 @@ const MainRouter = () => (
         <Switch>
             <Route exact path="/" component={Signup} />
             <Route exact path="/Pokemon" component={Pokemon} />
+            <Route exact path="/Function1" component={Function1} />
+            <Route exact path="/Function2" component={Function2} />
+            <Route exact path="/Function3" component={Function3} />
         </Switch>
     </div>
 );
